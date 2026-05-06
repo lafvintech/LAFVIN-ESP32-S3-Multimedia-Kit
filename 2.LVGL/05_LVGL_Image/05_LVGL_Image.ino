6b2ca63cac6d6ba204a4b93495c4129567f9677e
NightOwl <tech_edu_service@outlook.com>
2026-05-06 14:17:45 +0800

    fix

--- 2.LVGL/05_LVGL_Image/05_LVGL_Image.ino
+++ 2.LVGL/05_LVGL_Image/05_LVGL_Image.ino
@@ -26,7 +26,7 @@ void setup() {
     
     Serial.println("Mode: Embedded Image (C Array)");
     
-    Create picture object
+    // Create picture object
     lv_obj_t * img = lv_img_create(lv_scr_act());
     lv_img_set_src(img, &StarryNight);  // Set the image source to embedded data (240x320 StarryNight image)
     lv_obj_align(img, LV_ALIGN_CENTER, 0, 0);  // Center display
